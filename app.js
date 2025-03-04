01c49d648c7fb78755bee031ad294e8cd5684d28
Mahesh Aggarwal <developermahesh21@gmail.com>
2025-03-04 14:50:29 +0530

    Little UI changes

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-started = false;
-gameSeq = [];
-userSeq = [];
+let gameSeq = [];
+let userSeq = [];
+
+let started = false;
+let level = 0;
 
 document.addEventListener("keypress", function() {
     if(!started) {
